af892f7e252f196cbb72aae543055f44c6918289
itsguxt <itsguxt@gmail.com>
2023-09-29 11:54:09 -0300

    Classe Aluno

--- src/App.java
+++ src/App.java
@@ -1,7 +1,34 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, mundo!"); 
-        
+
+        //aprovado
+        Aluno aluno = new Aluno();
+        aluno.nome = "Gustavo";
+        aluno.notaMateria1 = 7;
+        aluno.notaMateria2 = 7;
+        aluno.notaMateria3 = 7;
+        aluno.faltas = 20;
+        aluno.checarAprovacao();
+
+        //reprovado por nota
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Gustavo";
+        aluno1.notaMateria1 = 7;
+        aluno1.notaMateria2 = 7;
+        aluno1.notaMateria3 = 6;
+        aluno1.faltas = 20;
+        aluno1.checarAprovacao();
+
+        //reprovado por falta
+        Aluno aluno2 = new Aluno();
+        aluno2.nome = "Gustavo";
+        aluno2.notaMateria1 = 7;
+        aluno2.notaMateria2 = 7;
+        aluno2.notaMateria3 = 7;
+        aluno2.faltas = 21;
+
+        aluno2.checarAprovacao();
+
     }
 
 }
